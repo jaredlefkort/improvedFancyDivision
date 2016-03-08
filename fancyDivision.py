1774a1abf0b9d1def033caf5883d8f6242aa2913
Jared <Jared@Jareds-MacBook-Pro.local>
2016-03-07 20:29:36 -0500

    added a function to ask the user if they would like to continue with different numbers

--- fancyDivision.py
+++ fancyDivision.py
@@ -1,19 +1,29 @@
 #Author: Jared Lefkort
 #Date: 2/4/2016
 
-dividend = int(input("Enter a dividend. "))
-
-divisor = int(input("Enter a divisor. "))
-#These are the inputs
+askAgain = "y"
 
 
-if dividend%divisor == 0:
-	print("{} divided by {} is ".format(dividend, divisor) + str(dividend/divisor))
+while askAgain == "y":
 
-	#this if statement will work for the exact multiples
+	dividend = int(input("Enter a dividend. "))
 
-else:
-	print("{} divided by {} is ".format(dividend, divisor) + str(dividend//divisor) + " with a remainder of " + str(dividend%divisor))
+	divisor = int(input("Enter a divisor. "))
+	#These are the inputs
 
-	#this statement will work when their is a remainder
+	quotient = dividend // divisor
+
+	remainder = dividend % divisor 
+	
+	if remainder == 0:
+		print("{} divided by {} is ".format(dividend, divisor) + str(quotient))
+		askAgain = input("Would you like to go again? (y/n): ")
+
+		#this if statement will work for the exact multiples
+
+	else:
+		print("{} divided by {} is ".format(dividend, divisor) + str(quotient) + " with a remainder of " + str(remainder))
+		askAgain = input("Would you like to go again? (y/n): ")
+
+		#this statement will work when their is a remainder
 
